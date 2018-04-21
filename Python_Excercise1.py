14ee04068baf6775a9329bb93637dd61bbe3f2ed
msam04 <msam04@users.noreply.github.com>
2018-04-21 02:18:27 -0400

    Initial commit

---
+++ Python_Excercise1.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+np.random.seed(0)
+x1 = np.random.randint(10, size = 6)
+x2 = np.random.randint(10, size = (3, 4))
+x3 = np.random.randint(10, size = (3, 4, 5))
+print(x1)
+print(x2)
+print(x3)
+
